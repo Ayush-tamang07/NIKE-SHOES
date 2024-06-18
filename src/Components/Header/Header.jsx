e015d7bcdc9b37f3d456a193526d00f8b1c37b19
Ayush Tamang <tamangayush052@gmail.com>
2024-06-18 14:38:55 +0545

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faHeart, faSearch} from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faHeart, faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 function Header() {
   return (
     <header>
-      <img className="nike"
+      <img
+        className="nike"
         src="https://cdn.mos.cms.futurecdn.net/786NzPhpXo6G8hkutJkHTM-1200-80.jpg"
         alt="logo"
         width={58.9633}
       />
-      <ul>
+      <ul className="nav-links">
         <li><a href="#">New & Featured</a></li>
         <li><a href="#">Men</a></li>
         <li><a href="#">Women</a></li>
@@ -19,14 +20,13 @@ function Header() {
       </ul>
       <div className="search-container">
         <FontAwesomeIcon icon={faSearch} className="search-icon" />
-        <input type="text" placeholder="Search" className="search"/>
+        <input type="text" placeholder="Search" />
       </div>
       <div className="icons">
         <FontAwesomeIcon icon={faHeart} className="cart-icon" />
         <FontAwesomeIcon icon={faCartShopping} className="cart-icon" />
       </div>
     </header>
-    
   );
 }
 
