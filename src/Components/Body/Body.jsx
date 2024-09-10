b67d7cb878547c19760d50ccde5ee54ac999cff1
Ayush Tamang <tamangayush052@gmail.com>
2024-09-10 15:20:16 +0545

    used prps

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -14,6 +14,7 @@ function Body() {
             </label>
           </div>
 
+
           <a href="#">Football</a>
           <a href="#">BasketBall</a>
           <a href="#">Running</a>
@@ -33,9 +34,56 @@ function Body() {
         </div>
 
         <div className="main">
-          <Card />
-          <Card />
-          <Card />
+          {/* <Card img  /> */}
+          <Card
+            cardMessage="Just In"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4b35bbe4-0dae-4160-91c1-3ada6ca52406/converse-chuck-70-high-top-unisex-shoe-hMF44R.png"
+            shoeName="Nike Pegasus 4"
+            shoesType="Men's Road Running Shoes"
+            colors="8 Colors"
+            price="150"
+          />
+          <Card
+            cardMessage="Just In"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1f31a5fd-9f59-4331-8bd1-c8fabe073b4e/zoom-vomero-5-shoes-MgsTqZ.png"
+            shoeName="Nike Pegasus 41 Blueprint"
+            shoesType="Men's Road Running Shoes"
+            colors="16 Colors"
+            price="$140"
+          />
+          <Card
+            cardMessage="Sustainable Materials"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b9c52ab5-ed2b-41e2-ba82-c395efcbd1e6/dunk-low-mens-shoes-l12Bc1.png"
+            shoeName="Nike Dunk Low"
+            shoesType="Men's Shoes"
+            colors="2 Colors"
+            price="$125"
+          />
+          <Card
+            cardMessage="Just In"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4b35bbe4-0dae-4160-91c1-3ada6ca52406/converse-chuck-70-high-top-unisex-shoe-hMF44R.png"
+            shoeName="Nike Pegasus 4"
+            shoesType="Men's Road Running Shoes"
+            colors="8 Colors"
+            price="150"
+          />
+          <Card
+            cardMessage="Just In"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1f31a5fd-9f59-4331-8bd1-c8fabe073b4e/zoom-vomero-5-shoes-MgsTqZ.png"
+            shoeName="Nike Pegasus 41 Blueprint"
+            shoesType="Men's Road Running Shoes"
+            colors="16 Colors"
+            price="$140"
+          />
+          <Card
+            cardMessage="Sustainable Materials"
+            img="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b9c52ab5-ed2b-41e2-ba82-c395efcbd1e6/dunk-low-mens-shoes-l12Bc1.png"
+            shoeName="Nike Dunk Low"
+            shoesType="Men's Shoes"
+            colors="2 Colors"
+            price="$125"
+          />
+          {/* <Card img="" shoeName="" shoesType="" colors="" price="" /> */}
         </div>
       </div>
     </>
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import "./Card.css";
-function Card() {
+function Card(props) {
   return (
     <>
       <div className="card-container">
@@ -7,45 +7,15 @@ function Card() {
           <a href="#">
             <img
               className="card-image"
-              src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4b35bbe4-0dae-4160-91c1-3ada6ca52406/converse-chuck-70-high-top-unisex-shoe-hMF44R.png"
+              src={props.img}
               alt="image"
               width={380}
             />
-            <div className="justin">Just In</div>
-            <div className="shoesName">Nike Pegasus 41</div>
-            <div className="shoesType">Men's Road Running Shoes</div>
-            <div className="colors">8 Colors</div>
-            <h3>$150</h3>
-          </a>
-        </div>
-        <div className="card">
-          <a href="#">
-            <img
-              className="card-image"
-              src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4b35bbe4-0dae-4160-91c1-3ada6ca52406/converse-chuck-70-high-top-unisex-shoe-hMF44R.png"
-              alt="image"
-              width={380}
-            />
-            <div className="justin">Just In</div>
-            <div className="shoesName">Nike Pegasus 41</div>
-            <div className="shoesType">Men's Road Running Shoes</div>
-            <div className="colors">8 Colors</div>
-            <h3>$150</h3>
-          </a>
-        </div>
-        <div className="card">
-          <a href="#">
-            <img
-              className="card-image"
-              src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4b35bbe4-0dae-4160-91c1-3ada6ca52406/converse-chuck-70-high-top-unisex-shoe-hMF44R.png"
-              alt="image"
-              width={380}
-            />
-            <div className="justin">Just In</div>
-            <div className="shoesName">Nike Pegasus 41</div>
-            <div className="shoesType">Men's Road Running Shoes</div>
-            <div className="colors">8 Colors</div>
-            <h3>$150</h3>
+            <div className="justin">{props.cardMessage}</div>
+            <div className="shoesName">{props.shoeName}</div>
+            <div className="shoesType">{props.shoesType}</div>
+            <div className="colors">{props.colors}</div>
+            <h3 className="price">{props.price}</h3>
           </a>
         </div>
       </div>
